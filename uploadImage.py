e9eafbd01ea6eb592938ad9e4e4d25dde89ceabd
veerubhotla-mohan <63038719+veerubhotla-mohan@users.noreply.github.com>
2022-09-17 07:38:01 +0530

    Add files via upload

--- uploadImage.py
+++ uploadImage.py
@@ -1,10 +1,10 @@
 import requests
-apiGatewayUrl = "<URL OF THE API GATEWAY>"
+apiGatewayUrl = "https://g3ycrq1bqh.execute-api.ap-south-1.amazonaws.com/dev/"
 s3BucketName = 'montycloudcoding/'
 
 
 def uploadImageToS3(image):
-    invocationString = apiGatewayUrl + s3BucketName + imageToBeUploaded
+    invocationString = apiGatewayUrl + s3BucketName + image
     print(invocationString)
     r = requests.put(invocationString)
     print(r)
